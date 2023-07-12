846bcbd1138c0942ee0b8382b1a7c241d5c8a3e3
Eagering Dev <eageringdev@gmail.com>
2023-07-12 16:34:19 +0900

    add generics

---
+++ generic/src/main.rs
@@ -0,0 +1,42 @@
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+pub mod aggregator;
+pub mod pair;
+
+use aggregator::{ Summary, Tweet, notify };
+use pair::Pair;
+
+fn main() {
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    notify(&tweet);
+
+    let pair = Pair::new(12, 20);
+    pair.cmp_display();
+}
